378065882e532cf340891c53556fb7e82ce3ae3b
gabrielebottari <gabriele.bottari1@gmail.com>
2023-12-04 16:52:58 +0100

    END exercise with styling and some bonus

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
 /*
+Scrivere un programma che chieda all'utente:
 
-Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+- Il numero di chilometri da percorrere
+- Età del passeggero
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
 
-il prezzo del biglietto è definito in base ai km (0.21 € al km)
-va applicato uno sconto del 20% per i minorenni
-va applicato uno sconto del 40% per gli over 65.
-L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+
+MILESTONE 1:
+Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà da scrivere in console.
+
+MILESTONE 2:
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l'utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
+Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
 */
 
@@ -14,11 +22,15 @@ const myButton = document.getElementById('generate-ticket');
 
 myButton.addEventListener('click', function() {
 
+    //-nome passeggero
+
     const nameInput = document.getElementById('nameInput');
 
     console.log('nameInput', nameInput, typeof nameInput);
     console.log('nome passegero', nameInput.value, typeof nameInput.value);
 
+    //- Il numero di chilometri da percorrere
+    
     //const kmInput = document.getElementById('kmInput');
     //const kmToTravel = parseInt(kmInput.value);
 
@@ -26,6 +38,8 @@ myButton.addEventListener('click', function() {
 
     console.log('kmInput',kmInput , typeof kmInput);
     console.log('km da percorrere', kmToTravel, typeof kmToTravel);
+
+    //- Età del passeggero
 
     //const ageInput = document.getElementById('ageInput');
     //const passengerAge = parseInt(ageInput.value);
@@ -36,9 +50,12 @@ myButton.addEventListener('click', function() {
     console.log('Età del passeggero', passengerAge, typeof passengerAge);
 
     
-
+    //- il prezzo del biglietto è definito in base ai km (0.21 € al km)
     let kmPrice = 0.21;
     let totalPrice = kmToTravel * kmPrice;
+
+    //- va applicato uno sconto del 20% per i minorenni
+    //- va applicato uno sconto del 40% per gli over 65.
 
     if (!isNaN(passengerAge)) {
 
@@ -60,11 +77,17 @@ myButton.addEventListener('click', function() {
 
     console.log('Il prezzo del tuo biglietto è: ' + totalPrice + '€');
 
-    document.getElementById('result').innerHTML=`Il prezzo del tuo biglietto è: ${totalPrice}€`;
+    document.getElementById('passenger-name').innerHTML= `${nameInput.value}`;
+
+    document.getElementById('result').innerHTML=`${totalPrice}€`;
 
     nameInput.value = '';
     kmInput.value = '';
     ageInput.value = '';
+
+    document.getElementById('class-ticket').innerHTML = 'Biglietto Standard' ;
+    document.getElementById('carriage').innerHTML = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    document.getElementById('code-number').innerHTML = Math.floor(Math.random() * 10000);
 
 });
 
